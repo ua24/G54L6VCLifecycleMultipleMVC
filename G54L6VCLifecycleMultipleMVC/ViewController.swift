34b588a5327c86f9a9b56ec19fb597d50652139a
Ivan Vasilevich <ivanvbesarab@gmail.com>
2017-05-12 20:01:59 +0300

    ViewController Lifecycle methods + superLog

--- G54L6VCLifecycleMultipleMVC/ViewController.swift
+++ G54L6VCLifecycleMultipleMVC/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
+func log(_ functionName: String = #function, line: Int = #line, file: String = #file) {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "HH:mm:ss"
+    print("\(dateFormatter.string(from: Date())) l#:\(line) \(functionName) in \((file as NSString).lastPathComponent)")
+}
+
 class ViewController: UIViewController {
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        log()
+    }
+    
+//    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+//        <#code#>
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        log()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        log()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        log()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        log()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        log()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        log()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        log()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        log()
     }
-
 
 }
 
